5e228933ef13eb56e32a3ee34cdd160be65d6805
Sampath <sampathpranay02@gmail.com>
2021-06-06 18:46:23 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
